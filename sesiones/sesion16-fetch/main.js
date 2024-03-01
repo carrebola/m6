e4c95fa00d2f6c22b1efca8bfae2a528842f484a
2223 Carlos Arrebola <carrebola2223@gmail.com>
2024-03-01 18:04:08 +0100

    .

--- sesiones/sesion16-fetch/main.js
+++ sesiones/sesion16-fetch/main.js
@@ -1,32 +1,32 @@
-// console.log('Hola Fetch');
+console.log('Hola Fetch');
 
-// // Peticion fetch a pokeapi
+// Peticion fetch a pokeapi
 
-// fetch('https://pokeapi.co/api/v2/pokemon')
-// .then(resp => resp.json())
-// .then(respJSON => {
-//   console.log('respuesta pokemons: ', respJSON);
-//   console.log('Nombre primer pokemon:', respJSON.results[0].name, respJSON.results[0].url);
+fetch('https://pokeapi.co/api/v2/pokemon')
+.then(resp => resp.json())
+.then(respJSON => {
+  console.log('respuesta pokemons: ', respJSON);
+  console.log('Nombre primer pokemon:', respJSON.results[0].name, respJSON.results[0].url);
 
-//   fetch(respJSON.results[0].url)
-//   .then(resp => resp.json())
-//   .then(resp2JSON =>{
-//     console.log('pokemon obtenido: ',resp2JSON);
-//     console.log('imagen', resp2JSON.sprites.front_default);
-//   })
-// })
+  fetch(respJSON.results[0].url)
+  .then(resp => resp.json())
+  .then(resp2JSON =>{
+    console.log('pokemon obtenido: ',resp2JSON);
+    console.log('imagen', resp2JSON.sprites.front_default);
+  })
+})
 
-// function inyectarPokemon(url){
-// // Codigo para añadir elementos a un div
-// const divPokemons = document.querySelector('#pokemons');
-// // Creamos elemento nuevo
-// const imagen = document.createElement("div");
-// // Le inyectamos la imagen
-// imagen.innerHTML = `<img src="${url}"></img>`
+function inyectarPokemon(url){
+// Codigo para añadir elementos a un div
+const divPokemons = document.querySelector('#pokemons');
+// Creamos elemento nuevo
+const imagen = document.createElement("div");
+// Le inyectamos la imagen
+imagen.innerHTML = `<img src="${url}"></img>`
 
-// // Añadimos en el div divPokemons el div creado con la imagen
-// divPokemons.appendChild(imagen)
-// }
+// Añadimos en el div divPokemons el div creado con la imagen
+divPokemons.appendChild(imagen)
+}
 
 const url = 'https:/pokapi.co/api/v2/pokemon'
 
